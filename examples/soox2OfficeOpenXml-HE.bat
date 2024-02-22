86855851ee6b1280cc1438bd90e8bb3002ed3e41
Olivier XILLO <oxillo@hotmail.com>
2024-02-22 23:20:11 +0100

    example upgrade

--- examples/soox2OfficeOpenXml-HE.bat
+++ examples/soox2OfficeOpenXml-HE.bat
@@ -1,8 +1,8 @@
 
 @set SOOX_HOME=%~dp0\..
 
-@set CLASSPATH=%SOOX_HOME%\SaxonHE12-3J\saxon-he-12.3.jar;
-@set CLASSPATH=%CLASSPATH%;%SOOX_HOME%\SaxonHE12-3J\soox-extensions.jar
+@set CLASSPATH=%SOOX_HOME%\SaxonHE12-4J\saxon-he-12.4.jar;
+@set CLASSPATH=%CLASSPATH%;%SOOX_HOME%\SaxonHE12-4J\soox-extensions.jar
 
 ::java net.sf.saxon.Transform  -it -config:../src/saxon-HE.config -xsl:test-extension.xsl -o:result.xml
 java net.sf.saxon.Transform  -it -config:../src/saxon-HE.config -xsl:soox2OfficeOpenXml.xsl -o:result.xml
